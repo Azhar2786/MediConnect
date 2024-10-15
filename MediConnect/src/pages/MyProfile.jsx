c7f20cb9d7697a197252aea8fc44786447ba9dc9
Azhar <darkcoders0101@gmail.com>
2024-10-15 23:45:30 +0530

    Adding Login code and my profile code

--- MediConnect/src/pages/MyProfile.jsx
+++ MediConnect/src/pages/MyProfile.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name: "Azhar",
+    image: assets.profile_pic,
+    email: 'darkdevils7265@gmail.com',
+    phone: '+1 123 456 789',
+    address: {
+      line1: '45 cross. Mohali',
+      line2: 'Chandigarh'
+    },
+    gender: 'Male',
+    dob: '15-12-2003'
+  })
+
+  const [isEdit, setIsEdit] = useState(false);
+
   return (
-    <div></div>
+    <div>
+
+      <img src={userData.image} alt="" />
+
+      {
+        isEdit
+        ? <input type="text" value={userData.name} onChange={e => setUserData(prev => ({...prev,name:e.target.value}))} />
+        : <p>{useState.name}</p>
+      }
+
+      <hr />
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>Email Id:</p>
+          <p>{userData.email}</p>
+          <p>Phone:</p>
+          {
+            isEdit
+            ? <input type="text" value={userData.phone} onChange={e => setUserData(prev => ({...prev,phone:e.target.value}))} />
+            : <p>{userData.phone}</p>
+          }
+          <p>Address:</p>
+          {
+            isEdit
+            ? <p>
+              <input onChange={(e) => setUserData ( prev => ({...prev, address: { ...prev.address, line1: e.target.value}}))} value={userData.address.line1}type="text" />
+              <br />
+              <input onChange={(e) => setUserData ( prev => ({...prev, address: { ...prev.address, line2: e.target.value}}))} value={userData.address.line2} type="text" />
+            </p>
+            : <p>
+              {userData.address.line1}
+              <br />
+              {userData.address.line2}
+            </p>
+          }
+        </div>
+      </div>
+
+    </div>
   )
 }
 
