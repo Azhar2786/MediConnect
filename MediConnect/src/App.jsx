ec72589861159d758a57df7a6f6451cc2df44a73
Azhar <darkcoders0101@gmail.com>
2024-12-14 12:13:57 +0530

    adding remaing file

--- MediConnect/src/App.jsx
+++ MediConnect/src/App.jsx
@@ -13,7 +13,7 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className='mx-4 sm:max-[10%]'>
+    <div className='mx-9 sm:max-[10%]'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
